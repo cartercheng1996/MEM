829726f4feda1a8b0972806a2d8df9ecabadd5cc
Zihao Carter Cheng <chinesecarter@gmail.com>
2020-02-20 10:38:12 +1100

    finished Memory model

--- MEM.srcs/sources_1/new/register_fsm.vhd
+++ MEM.srcs/sources_1/new/register_fsm.vhd
@@ -1,27 +1,43 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2020/02/20 10:36:52
+-- Design Name: 
+-- Module Name: register_fsm - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity register_fsm is
-  port (
-    clk          : in  std_logic;
-    reset        : in  std_logic;
-    --
-    data_in      : in  std_logic_vector(7 downto 0);
-    data_in_vld  : in  std_logic;
-    data_out     : out std_logic_vector(7 downto 0);
-    data_out_vld : out std_logic;
-    --
-    addr         : out std_logic_vector(15 downto 0);
-    wr_data      : out std_logic_vector(31 downto 0);
-    wr_en        : out std_logic;
-    rd_en        : out std_logic;
-    rd_data      : in  std_logic_vector(31 downto 0);
-    rd_ack       : in  std_logic);
+--  Port ( );
 end register_fsm;
 
-architecture rtl of register_fsm is
+architecture Behavioral of register_fsm is
 
 begin
 
-end rtl;
+
+end Behavioral;
